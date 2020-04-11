5503257cf66c758f7fd32b934eb1a91d03793ef0
Andre Vitorino <andregnr163@gmail.com>
2020-04-11 14:48:23 -0300

    projeto criado

---
+++ src/main/java/com/teste/mongo/demo/TestemongoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.mongo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestemongoApplication.class, args);
+	}
+
+}
