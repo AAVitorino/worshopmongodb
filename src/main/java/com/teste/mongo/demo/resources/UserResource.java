0f599810e675e3bd92adacb75ef27839e1bcf6ef
Andre Vitorino <andregnr163@gmail.com>
2020-04-28 19:41:51 -0300

    Metodo Deletebyid

--- src/main/java/com/teste/mongo/demo/resources/UserResource.java
+++ src/main/java/com/teste/mongo/demo/resources/UserResource.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,15 @@ public class UserResource {
 	
 		
 		
+	}
+	@RequestMapping(value="/{id2}")
+	@DeleteMapping
+	public ResponseEntity<Void>delete(@PathVariable String id) {
+		service.delete(id);
+		
+		
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	
